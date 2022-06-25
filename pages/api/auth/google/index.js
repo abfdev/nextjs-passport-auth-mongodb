39a5a96ea29e7c80ed4a93789f442219ed91acc0
hh <h>
2022-06-25 16:40:25 +0100

    refactoring code and fix errors vercel

--- pages/api/auth/google/index.js
+++ pages/api/auth/google/index.js
@@ -2,7 +2,7 @@ import googleStrategy from "@lib/passport/googleStrategy";
 import withPassport, { passport } from "@lib/withPassport";
 
 const handler = async (req, res) => {
-  passport.authenticate("google", {
+  await passport.authenticate("google", {
     scope: ["profile", "email"],
   })(req, res);
 };
